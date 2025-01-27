15c61bef408584ecca8e1793600a2239700dccb3
Esteban Lopez 👋 <esteban.steam21@gmail.com>
2025-01-27 16:59:28 +0900

    Create Initials.swift

---
+++ swift/Initials.swift
@@ -0,0 +1,16 @@
+//"Fun Fact : I can speak three langauges, "English, Spanish and Japanese!"
+
+print("EEEEE")
+print("E    ")
+print("E    ")
+print("EEEEE")
+print("E    ")
+print("E    ")
+print("EEEEE")
+
+print("L    ")
+print("L    ")
+print("L    ")
+print("L    ")
+print("L    ")
+print("LLLLLL")
